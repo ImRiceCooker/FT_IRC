a973c9e97be14f0284f93d41cbf304786663fabf
Park-Jeong-Ki <jgpark1998@gmail.com>
2023-05-10 19:37:13 +0900

    NOTICE P2P fix

--- srcs/Database_users.cpp
+++ srcs/Database_users.cpp
@@ -462,11 +462,20 @@ Udata Database::command_notice(const uintptr_t &ident, const std::string &target
 	}
 	else
 	{
+		// if (is_user(target_name))
+		// {
+		// 	User &tar_usr = select_user(target_name);
+
+		// 	tmp = Sender::notice_p2p_message(cur_usr, tar_usr, msg);
+		// 	ret.insert(tmp);
+		// }
 		if (is_user(target_name))
 		{
 			User &tar_usr = select_user(target_name);
-
-			tmp = Sender::notice_p2p_message(cur_usr, tar_usr, msg);
+			tmp = Sender::privmsg_p2p_message(cur_usr, tar_usr, msg);
+			ret.insert(tmp);
+			tmp.first = cur_usr.client_sock_;
+			tmp.second.clear();
 			ret.insert(tmp);
 		}
 		else
