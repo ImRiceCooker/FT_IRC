394c6633908503a271a2bf51cf51fa548bebc48a
sielee <tldms975@naver.com>
2023-05-20 17:19:19 +0900

    add valid user check on set_topic

--- srcs/Database_channels.cpp
+++ srcs/Database_channels.cpp
@@ -295,15 +295,19 @@ Udata Database::nick_channel(User &nicker, std::string &send_msg)
 Udata Database::set_topic(User &sender, std::string &chan_name, std::string &topic)
 {
 	Udata ret;
-	Event tmp;
+	Event tmp = valid_user_checker_(sender.client_sock_, "INVITE");
+	Channel &channel = select_channel(chan_name);
 
-	if (is_channel(chan_name) == false)
+	if (tmp.second.size())
+	{
+		ret.insert(tmp);
+	}
+	else if (is_channel(chan_name) == false)
 	{
 		tmp = Sender::no_channel_message(sender, chan_name);
 		ret.insert(tmp);
 		return ret;
 	}
-	Channel &channel = select_channel(chan_name);
 	if ((channel.channel_flag_ & F_TOPIC_OWNERSHIP) && !(channel.is_host(sender)))
 	{
 		tmp = Sender::topic_access_error(sender, chan_name);
