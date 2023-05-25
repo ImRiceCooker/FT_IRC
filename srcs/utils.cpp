08c425f20154f90bb177e2b335be1474d2cb9c15
sielee <tldms975@naver.com>
2023-05-25 15:38:57 +0900

    change value name and file name along with git conf changed

--- srcs/utils.cpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include <iostream>
-#include "Color.hpp"
-
-void exit_with_perror(const std::string& msg)
-{
-	std::cerr << BOLDRED << msg << RESET << std::endl;
-	exit(EXIT_FAILURE);
-}
